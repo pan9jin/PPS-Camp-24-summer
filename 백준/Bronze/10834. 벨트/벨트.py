745281173aa768e094e5727461b65e01552a0b86
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-07-11 17:55:50 +0900

    [Bronze II] Title: 벨트, Time: 136 ms, Memory: 39424 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.py
+++ 백준/Bronze/10834. 벨트/벨트.py
@@ -1,8 +1,10 @@
+from fractions import Fraction
+
 n = int(input())
 d = 0
-r = 1
+r = Fraction(1)
 for _ in range(n):
     f, s, bd = map(int, input().split())
-    d = d if bd == 0 else not d
-    r *= (s / f)
-print(int(d), int(r))
+    d = (d + bd) % 2
+    r *= Fraction(s, f)
+print(d, int(float(r)))
