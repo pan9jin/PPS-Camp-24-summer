d05dbcf39bf8959d940c60a0cd0007e7bb034a00
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-07-11 21:07:26 +0900

    [Bronze II] Title: 3000번 버스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9546. 3000번 버스/3000번 버스.py
+++ 백준/Bronze/9546. 3000번 버스/3000번 버스.py
@@ -2,10 +2,7 @@ n = int(input())
 result = []
 for _ in range(n):
     st = int(input())
-    p = 0
-    for _ in range(st):
-        p += 0.5
-        p *= 2
+    p = (2 ** st) - 1
     result.append(int(p))
 for i in range(n):
     print(result[i])
