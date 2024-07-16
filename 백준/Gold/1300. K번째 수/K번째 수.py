fec3317253d7ced35e7f224a5b8113684bd4790d
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-07-16 21:24:32 +0900

    [Gold I] Title: K번째 수, Time: 1052 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n, k = int(input()), int(input())
+start, end = 1, k
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    tmp = 0
+    for i in range(1, n+1):
+        tmp += min(mid//i, n)
+    
+    if tmp >= k:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
