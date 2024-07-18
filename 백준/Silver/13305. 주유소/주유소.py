d1fd1d363d8744c141f8083ed7473be4e51e23ec
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-07-18 11:04:13 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,13 +3,10 @@ input = sys.stdin.readline
 n = int(input())
 road = list(map(int, input().split()))
 oil = list(map(int, input().split()))
-oil = oil[:-1]
-price = 0
+tot_price = 0
+price = oil[0]
 for i in range(n-1):
-    m_price = min(oil)
-    if oil[i] == m_price:
-        price += oil[i] * sum(road[i:n])
-        break
-    elif oil[i] > oil[i+1]:
-        price += oil[i] * road[i]
-print(price)
+    if price > oil[i]:
+        price = oil[i]
+    tot_price += price * road[i]
+print(tot_price)
