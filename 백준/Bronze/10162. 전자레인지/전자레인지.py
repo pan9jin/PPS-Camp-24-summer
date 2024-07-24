8869db79d6c2433f555c27ae236025f70799362c
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-07-24 21:13:18 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -2,18 +2,14 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
+button = [300, 60, 10]
 cnt = [0]*3
-while t > 0:
-    if t >= 300:
-        t -= 300
-        cnt[0] += 1
-    elif t >= 60:
-        t -= 60
-        cnt[1] += 1
-    elif t >= 10:
-        t -= 10
-        cnt[2] += 1
-    else:
-        print(-1)
-        exit(0)
-print(*cnt)
+
+for i in range(3):
+    cnt[i] = t // button[i]
+    t %= button[i]
+
+if t != 0:
+    print(-1)
+else:
+    print(*cnt)
