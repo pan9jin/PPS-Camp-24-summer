f50822cb1b4edefd1c8f557401e9ce62e6f512ee
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2024-08-02 11:03:39 +0900

    [Bronze II] Title: 분해합, Time: 1244 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,13 @@
+import sys
+import math
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(1, n+1):
+    num = sum((map(int, str(i))))
+    total = i + num
+    if total == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
