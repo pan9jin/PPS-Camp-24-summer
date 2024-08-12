6563f77ecdffa7c508a808b42784d4ada3f2fc81
GwangjinJeong <127232362+pan9jin@users.noreply.github.com>
2024-08-12 10:57:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 612 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == n:
+        break
+    result += 1
+
+print(result)
