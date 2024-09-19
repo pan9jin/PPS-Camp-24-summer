ca14947ec90ba4312adf3266523a090a86ab3b50
Gwangjin Jeong <127232362+pan9jin@users.noreply.github.com>
2024-09-19 14:35:35 +0900

    [Bronze III] Title: 두~~부 두부 두부, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+while k < 0:
+    k += n
+
+result = (((k - 3) % n) + m)
+if result > n:
+    result %= n
+print(result)
