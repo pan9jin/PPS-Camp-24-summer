e0d12c3c8668217afe8b0c5f014ba24c11008089
Gwangjin Jeong <127232362+pan9jin@users.noreply.github.com>
2024-10-31 11:26:26 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = input().rstrip()
+result = 0
+for i in range(1, len(n)):
+    result += 9*10**(i-1)*i
+result += (int(n) - 10**(len(n)-1)+1)*len(n)
+print(result)
